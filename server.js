0753b08d92922eabb1f3b4568dc9b3e159232220
thiago augusto ferreira da silva <thiagoaugusto1976@gmail.com>
2026-06-07 17:05:08 -0300

    1.0.5

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import { spawn } from 'child_process';
+import fs from 'fs';
+import path from 'path';
 
 const app = express();
+  const HLS_DIR = './hls';
+
+    if (!fs.existsSync(HLS_DIR)) {
+      fs.mkdirSync(HLS_DIR, { recursive: true });
+  }
 
 app.get('/proxy', async (req, res) => {
   try {
@@ -41,4 +49,76 @@ app.get('/proxy', async (req, res) => {
 
 app.listen(3000, () => {
   console.log('Proxy rodando na porta 3000');
+});
+
+app.use('/hls', express.static(HLS_DIR));
+
+const activeStreams = new Map();
+
+app.get('/stream', async (req, res) => {
+  try {
+    const sourceUrl = req.query.url;
+
+    if (!sourceUrl) {
+      return res.status(400).json({
+        error: 'URL obrigatória'
+      });
+    }
+
+    const streamId = Buffer
+      .from(sourceUrl)
+      .toString('base64')
+      .replace(/[^a-zA-Z0-9]/g, '');
+
+    const outputDir = path.join(HLS_DIR, streamId);
+
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir, { recursive: true });
+    }
+
+    const playlistPath = path.join(outputDir, 'index.m3u8');
+
+    if (!activeStreams.has(streamId)) {
+
+      console.log('Iniciando FFmpeg:', streamId);
+
+      const ffmpeg = spawn('ffmpeg', [
+        '-i', sourceUrl,
+
+        '-c:v', 'copy',
+        '-c:a', 'aac',
+
+        '-f', 'hls',
+
+        '-hls_time', '4',
+        '-hls_list_size', '6',
+
+        '-hls_flags',
+        'delete_segments+append_list',
+
+        playlistPath
+      ]);
+
+      ffmpeg.stderr.on('data', data => {
+        console.log(data.toString());
+      });
+
+      ffmpeg.on('close', code => {
+        console.log('FFmpeg encerrado:', code);
+        activeStreams.delete(streamId);
+      });
+
+      activeStreams.set(streamId, ffmpeg);
+    }
+
+    res.json({
+      hls: `https://proxy.silvatech.dev.br/hls/${streamId}/index.m3u8`
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      error: err.message
+    });
+  }
 });
